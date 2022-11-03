f8817df920d664362ee2e1d2e035f1e63158abd0
dhvanilambate21 <dhvanilambate01@gmail.com>
2022-11-03 12:23:50 +0530

    remove token and data store using stringify

--- app/Http/Controllers/FormDataController.php
+++ app/Http/Controllers/FormDataController.php
@@ -8,11 +8,19 @@ use App\Models\FormData;
 class FormDataController extends Controller
 {
     function index(Request $request){
-        $formData = new FormData;
-        $formData->website = $request->input('website');
-        $formData->data =json_encode($request ->data);
-        $formData->save();
+        $this->validate(request(),[
+            'website'=>'required|max:255',
+            'data'=>'required',
+        ]);
 
+    
+        $formData = new FormData;
+        $data = $request->all(); 
+        // dd($data['data']);
+        $formData->website = $request->input('website');
+        $formData->data =json_encode($data['data']);      
+        $formData->save();
+        
         return response()->json($formData);
     }
 }
--- app/Models/FormData.php
+++ app/Models/FormData.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FormData extends Model
-{
+{   
+    // use HasApiTokens;
     protected $table = "form_data";
     protected $fillable = ['website', 'data'];
 }
